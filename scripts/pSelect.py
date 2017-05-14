b347af3199f694afe86db8163f1993e36adc009d
Mayank Bhushan <miku.bhushan@gmail.com>
2017-05-15 03:59:15 +0530

    Added botThread class

--- scripts/pSelect.py
+++ scripts/pSelect.py
@@ -1,5 +1,16 @@
+import rospy
 
-class pSelect:
-	def selectPlay(self,data):
-		# TO DO To write select play and return it instead of none 
-		return None
+def refCallback(data):
+	global refStatus
+	refStatus = data
+
+def selectPlay(self,data):
+	
+	if refStatus :  # TO DO To check whether need to use ref plays
+		return refStatus
+	else :
+		return None  # TO DO To write select play and return it instead of none 
+
+if __name__=='__main__':
+	rospy.init_node('ref_play_node', anonymous=True)
+	rospy.Subscriber('/ref_state', BeliefState, refCallback)
--- scripts/pmain.py
+++ scripts/pmain.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python
-
+import threading
 import rospy
-from pSelect import pSelect
+import pSelect
 from krssg_ssl_msgs.msg import TacticPacket
 from krssg_ssl_msgs.msg import BeliefState
 
+thread_running = [False,False,False,False,False,False] # Global variable to check whichever threads are currently running
+
+class botThread (threading.Thread):
+	def __init__(self,botID,name, tacticID):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.botID = botID
+		self.tacticID = tacticID
+		# need to check for threadID
+
+	def run(self):
+		pass # depending on tactic ID need to call the tactic and tactic needs on completion needs to execute threadName.exit()
+			# so threadName needs to be passed to tactic
+			# we can also modify the role node to pass state and bot param and get tactic returned
+
 state = BeliefState()
+
 def Callback(data):
 	rospy.loginfo('Received frame: {}'.format(data.frame_number))
-	pselect = pSelect()
-	play_name = pselect.selectPlay(data)
-	
-rospy.init_node('play_py_node', anonymous=True)
-rospy.Subscriber('/belief_state', BeliefState, Callback)
-rospy.spin()
+	play_name = pSelect.selectPlay(data)
+	if play_name : # TO DO check if refree play then use a refree play
+		pass
+	else :
+		pass # TO DO the completed threads needs to be assigned a tactic and run using botThread object
+
+if __name__=='__main__':
+	rospy.init_node('play_py_node', anonymous=True)
+	rospy.Subscriber('/belief_state', BeliefState, Callback)
+	rospy.spin()
